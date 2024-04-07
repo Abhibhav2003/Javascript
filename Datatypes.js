b9b511f1de2b8733515fcafdb2834b230c3c47ef
Abhibhav Sharma <139039304+Abhibhav2003@users.noreply.github.com>
2024-04-07 06:02:44 +0000

    Datatypes

---
+++ Datatypes.js
@@ -0,0 +1,20 @@
+"use strict";
+
+//alert("Hello");//using nodejs not browser
+
+//Datatypes
+
+// number => Range: -(2^53-1) to +(2^53-1)
+// bigint
+// string
+// boolean
+// null
+// undefined
+// symbol
+
+//object
+
+console.log(typeof "hello") //=>string
+console.log(typeof null) // => object
+console.log(typeof undefined) //=> undefined
+
